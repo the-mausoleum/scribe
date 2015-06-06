7dac5da654404292b78ff0f33cc45aec16533bd2
Marshall Bowers <elliott.codes@gmail.com>
2015-06-05 23:57:11 -0400

    Improve commit parsing

--- modules/commit.go
+++ modules/commit.go
@@ -1,9 +1,11 @@
 package modules
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type commit struct {
@@ -12,18 +14,35 @@ type commit struct {
 	message string
 }
 
-func parseArgs(args []string) commit {
+func parseArgs(args []string) (commit, error) {
+	if len(args) < 2 {
+		return commit{}, errors.New("Not enough arguments.")
+	}
+
+	var project string = args[0]
 	hours, err := strconv.Atoi(args[1])
+
+	if err != nil {
+		return commit{}, err
+	}
+
+	var message string
+
+	if len(args) > 2 {
+		message = strings.Join(args[2:], " ")
+	} else {
+		message = ""
+	}
+
+	return commit{project, hours, message}, nil
+}
+
+func Commit(args []string) {
+	commit, err := parseArgs(args)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
-
-	return commit{args[0], hours, args[2]}
-}
-
-func Commit(args []string) {
-	commit := parseArgs(args)
 
 	fmt.Println(commit)
 }
