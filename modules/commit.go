7e50029c650e6497ecdc140740e4250fc4450be7
Marshall Bowers <elliott.codes@gmail.com>
2015-06-05 22:20:39 -0400

    Prettify

--- modules/commit.go
+++ modules/commit.go
@@ -2,28 +2,28 @@ package modules
 
 import (
 	"fmt"
-    "os"
-    "strconv"
+	"os"
+	"strconv"
 )
 
 type commit struct {
-    project string
-    hours int
-    message string
+	project string
+	hours   int
+	message string
 }
 
 func parseArgs(args []string) commit {
-    hours, err := strconv.Atoi(args[1])
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(0)
-    }
+	hours, err := strconv.Atoi(args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
 
-    return commit{args[0], hours, args[2]}
+	return commit{args[0], hours, args[2]}
 }
 
 func Commit(args []string) {
-    commit := parseArgs(args)
+	commit := parseArgs(args)
 
-    fmt.Println(commit)
+	fmt.Println(commit)
 }
